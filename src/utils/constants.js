e14e6633c12d1ae31b95727f7c2b416cf46e1372
Sumit <joshiz.sumit@gmail.com>
2023-03-24 23:02:55 +0530

    fixed auto suggestions bug

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,11 @@
 import { generateRandomMessage, generateRandomName } from "./helper";
 
 export const YOUTUBE_API_KEY = "AIzaSyB1PCJm_8EU2QvFGC1mCRhP9pOL6-kNkaI";
+
 export const YOUTUBE_API_LINK = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key=${YOUTUBE_API_KEY}`;
+
 export const YOUTUBE_AUTO_SUGGESTIONS_API_LINK =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const LIVECHAT_OFFSET = 12;
 export const commentsData = [
